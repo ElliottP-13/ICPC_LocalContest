b4687f5c5eb161d0c21155470aee66d59a6eb234
Elliott P <pryorelliott@gmail.com>
2022-03-25 15:26:04 -0600

    Comments

--- Demo/demo.java
+++ Demo/demo.java
@@ -4,15 +4,17 @@ public class demo{
 
     public static void main(String[] args){
 
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);  // Scanner object to get input from standard in
 
-        String line = scan.nextLine();
-        int n = Integer.parseInt(line);
+        String line = scan.nextLine();  // read the first line
+        int n = Integer.parseInt(line);  // Turn it into an integer
 
-        for (int i = 0; i < n; i ++){
+        for (int i = 0; i < n; i ++){  // For the number of problems ...
 
-            line = scan.nextLine();
-            String[] split = line.split(" ");
+            line = scan.nextLine();  // read the line
+            String[] split = line.split(" ");  // split it so we can get a and b
+            
+            // parse a and b as ints
             int a = Integer.parseInt(split[0]);
             int b = Integer.parseInt(split[1]);
 
